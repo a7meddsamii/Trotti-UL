7d2bddd34c8265558ab7fb99b8f7543403758bd2
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-17 11:31:43 -0400

    fix typo

--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/config/binders/DummyInterface.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/config/binders/DummyInterface.java
@@ -1,7 +1,7 @@
 package ca.ulaval.glo4003.trotti.infrastructure.config.binders;
 
 /**
- * This class exists only for example purposes, it should be deleted
+ * This interface exists only for example purposes, it should be deleted
  */
 public interface DummyInterface {
 }
