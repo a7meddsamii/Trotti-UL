3b79995cd9cdfeabb35aea3916febad6f8fd2f6a
a7meddsamii <ahmed.sami.1@ulaval.ca>
2025-09-19 13:37:59 -0400

    add dumb test.

---
+++ src/test/java/ca/ulaval/glo4003/trotti/infrastructure/config/ServerResourceLocatorTest.java
@@ -0,0 +1,18 @@
+package ca.ulaval.glo4003.trotti.infrastructure.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerResourceLocatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+}
