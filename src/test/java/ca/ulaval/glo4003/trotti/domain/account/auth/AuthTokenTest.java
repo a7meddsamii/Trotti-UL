6609efd57cbdc5132ead37a895278ec5f0487d4f
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-20 23:49:31 -0400

    Run styling

--- src/test/java/ca/ulaval/glo4003/trotti/domain/account/auth/AuthTokenTest.java
+++ src/test/java/ca/ulaval/glo4003/trotti/domain/account/auth/AuthTokenTest.java
@@ -8,37 +8,37 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 class AuthTokenTest {
-	private final String TOKEN_VALUE = "token";
-	private final String DIFFERENT_TOKEN_VALUE = "differentToken";
-	private final Object OBJECT_OF_DIFFERENT_TYPE = new Object();
-	
-	@Test
-	void givenBlankValue_whenCreatingAuthToken_thenThrowException() {
-		Executable authTokenCreation = () -> AuthToken.from(StringUtils.EMPTY);
-		
-		Assertions.assertThrows(MalformedTokenException.class, authTokenCreation);
-	}
-	
-	@Test
-	void givenTwoTokenOfSameValue_whenComparing_thenTheyAreEqual() {
-		AuthToken authToken = AuthToken.from(TOKEN_VALUE);
-		AuthToken anotherAuthToken = AuthToken.from(TOKEN_VALUE);
-		
-		Assertions.assertEquals(authToken, anotherAuthToken);
-	}
-	
-	@Test
-	void givenTwoTokenOfDifferentValue_whenComparing_thenTheyAreNotEqual() {
-		AuthToken authToken = AuthToken.from(TOKEN_VALUE);
-		AuthToken anotherAuthToken = AuthToken.from(DIFFERENT_TOKEN_VALUE);
-		
-		Assertions.assertNotEquals(authToken, anotherAuthToken);
-	}
-	
-	@Test
-	void givenToken_whenComparingToDifferentType_thenTheyAreNotEqual() {
-		AuthToken authToken = AuthToken.from(TOKEN_VALUE);
-		
-		Assertions.assertNotEquals(OBJECT_OF_DIFFERENT_TYPE, authToken);
-	}
+    private final String TOKEN_VALUE = "token";
+    private final String DIFFERENT_TOKEN_VALUE = "differentToken";
+    private final Object OBJECT_OF_DIFFERENT_TYPE = new Object();
+
+    @Test
+    void givenBlankValue_whenCreatingAuthToken_thenThrowException() {
+        Executable authTokenCreation = () -> AuthToken.from(StringUtils.EMPTY);
+
+        Assertions.assertThrows(MalformedTokenException.class, authTokenCreation);
+    }
+
+    @Test
+    void givenTwoTokenOfSameValue_whenComparing_thenTheyAreEqual() {
+        AuthToken authToken = AuthToken.from(TOKEN_VALUE);
+        AuthToken anotherAuthToken = AuthToken.from(TOKEN_VALUE);
+
+        Assertions.assertEquals(authToken, anotherAuthToken);
+    }
+
+    @Test
+    void givenTwoTokenOfDifferentValue_whenComparing_thenTheyAreNotEqual() {
+        AuthToken authToken = AuthToken.from(TOKEN_VALUE);
+        AuthToken anotherAuthToken = AuthToken.from(DIFFERENT_TOKEN_VALUE);
+
+        Assertions.assertNotEquals(authToken, anotherAuthToken);
+    }
+
+    @Test
+    void givenToken_whenComparingToDifferentType_thenTheyAreNotEqual() {
+        AuthToken authToken = AuthToken.from(TOKEN_VALUE);
+
+        Assertions.assertNotEquals(OBJECT_OF_DIFFERENT_TYPE, authToken);
+    }
 }
