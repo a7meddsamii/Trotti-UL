a845eecd259b095002b971d4296009a5e7d66949
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-21 20:14:51 -0400

    Apply styling

--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/UserInMemoryDatabase.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/UserInMemoryDatabase.java
@@ -4,38 +4,36 @@ import ca.ulaval.glo4003.trotti.domain.account.Idul;
 import ca.ulaval.glo4003.trotti.domain.account.exceptions.AccountNotFoundException;
 import ca.ulaval.glo4003.trotti.infrastructure.repository.account.AccountEntity;
 import ca.ulaval.glo4003.trotti.infrastructure.repository.order.BuyerEntity;
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentMap;
 
 public class UserInMemoryDatabase {
     ConcurrentMap<Idul, AccountEntity> accountTable;
-	ConcurrentMap<Idul, BuyerEntity> buyerTable;
+    ConcurrentMap<Idul, BuyerEntity> buyerTable;
 
     public UserInMemoryDatabase(
-			ConcurrentMap<Idul, AccountEntity> accountTable,
-			ConcurrentMap<Idul, BuyerEntity> buyerTable) {
+            ConcurrentMap<Idul, AccountEntity> accountTable,
+            ConcurrentMap<Idul, BuyerEntity> buyerTable) {
         this.accountTable = accountTable;
         this.buyerTable = buyerTable;
     }
 
-	
-	public void insertIntoAccountTable(AccountEntity account) {
-		accountTable.put(account.idul(), account);
-	}
-	
-	public Optional<AccountEntity> selectFromAccountTable(Idul idul) {
-		return Optional.ofNullable(accountTable.get(idul));
-	}
+    public void insertIntoAccountTable(AccountEntity account) {
+        accountTable.put(account.idul(), account);
+    }
+
+    public Optional<AccountEntity> selectFromAccountTable(Idul idul) {
+        return Optional.ofNullable(accountTable.get(idul));
+    }
 
     public void insertIntoBuyerTable(BuyerEntity buyer) {
-		enforceAccountForeignKeyConstraint(buyer.idul());
+        enforceAccountForeignKeyConstraint(buyer.idul());
         buyerTable.put(buyer.idul(), buyer);
     }
-	
-	private void enforceAccountForeignKeyConstraint(Idul idul) {
-		if (!accountTable.containsKey(idul)) {
-			throw new AccountNotFoundException();
-		}
-	}
+
+    private void enforceAccountForeignKeyConstraint(Idul idul) {
+        if (!accountTable.containsKey(idul)) {
+            throw new AccountNotFoundException();
+        }
+    }
 }
--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/InMemoryAccountRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/InMemoryAccountRepository.java
@@ -5,34 +5,29 @@ import ca.ulaval.glo4003.trotti.domain.account.Idul;
 import ca.ulaval.glo4003.trotti.domain.account.exceptions.AccountNotFoundException;
 import ca.ulaval.glo4003.trotti.domain.account.repository.AccountRepository;
 import ca.ulaval.glo4003.trotti.infrastructure.repository.UserInMemoryDatabase;
-
 import java.util.Optional;
 
 public class InMemoryAccountRepository implements AccountRepository {
-	private final UserInMemoryDatabase database;
-	
-	public InMemoryAccountRepository(UserInMemoryDatabase database) {
-		this.database = database;
-	}
-	
-	@Override
-	public void save(Account account) {
-		AccountEntity accountEntity =
-				new AccountEntity(
-						account.getIdul(), account.getName(), account.getBirthDate(),
-						account.getGender(), account.getEmail(), account.getPassword()
-				);
-		this.database.insertIntoAccountTable(accountEntity);
-	}
-	
-	@Override
-	public Account findByIdul(Idul idul) {
-		Optional<AccountEntity> accountQuery = database.selectFromAccountTable(idul);
-		AccountEntity accountFound = accountQuery.orElseThrow(AccountNotFoundException::new);
-		
-		return new Account(
-				accountFound.name(), accountFound.birthDate(), accountFound.gender(),
-				accountFound.idul(), accountFound.email(), accountFound.password()
-		);
-	}
+    private final UserInMemoryDatabase database;
+
+    public InMemoryAccountRepository(UserInMemoryDatabase database) {
+        this.database = database;
+    }
+
+    @Override
+    public void save(Account account) {
+        AccountEntity accountEntity =
+                new AccountEntity(account.getIdul(), account.getName(), account.getBirthDate(),
+                        account.getGender(), account.getEmail(), account.getPassword());
+        this.database.insertIntoAccountTable(accountEntity);
+    }
+
+    @Override
+    public Account findByIdul(Idul idul) {
+        Optional<AccountEntity> accountQuery = database.selectFromAccountTable(idul);
+        AccountEntity accountFound = accountQuery.orElseThrow(AccountNotFoundException::new);
+
+        return new Account(accountFound.name(), accountFound.birthDate(), accountFound.gender(),
+                accountFound.idul(), accountFound.email(), accountFound.password());
+    }
 }
