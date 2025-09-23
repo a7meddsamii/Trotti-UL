f76024426293e94377f6babbcbf0dfbe314699b4
moussa <ali.moussa.1@ulaval.ca>
2025-09-23 10:08:30 -0400

    refactor: delete token port
    createAccount success case (repository.save called)
    createAccount with existing email throws InvalidParameterException
    createAccount with existing idul throws InvalidParameterException

--- src/main/java/ca/ulaval/glo4003/trotti/application/port/TokenPort.java
+++
@@ -1,7 +0,0 @@
-package ca.ulaval.glo4003.trotti.application.port;
-
-import ca.ulaval.glo4003.trotti.domain.account.Idul;
-
-public interface TokenPort {
-    String generateToken(Idul accountId);
-}
