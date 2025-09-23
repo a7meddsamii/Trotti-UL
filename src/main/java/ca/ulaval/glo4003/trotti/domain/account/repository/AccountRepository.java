f57e317deea3cf95b4079f4f5c6c019c1aceaad4
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-22 21:32:05 -0400

    Add remaining repository methods

--- src/main/java/ca/ulaval/glo4003/trotti/domain/account/repository/AccountRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/account/repository/AccountRepository.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.trotti.domain.account.repository;
 
 import ca.ulaval.glo4003.trotti.domain.account.Account;
+import ca.ulaval.glo4003.trotti.domain.account.Email;
 import ca.ulaval.glo4003.trotti.domain.account.Idul;
+import java.util.Optional;
 
 public interface AccountRepository {
     void save(Account account);
 
-    Account findByIdul(Idul idul);
+    Optional<Account> findByEmail(Email email);
+
+    Optional<Account> findByIdul(Idul idul);
 }
--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/UserInMemoryDatabase.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/UserInMemoryDatabase.java
@@ -1,5 +1,6 @@
 package ca.ulaval.glo4003.trotti.infrastructure.repository;
 
+import ca.ulaval.glo4003.trotti.domain.account.Email;
 import ca.ulaval.glo4003.trotti.domain.account.Idul;
 import ca.ulaval.glo4003.trotti.domain.account.exceptions.AccountNotFoundException;
 import ca.ulaval.glo4003.trotti.infrastructure.repository.account.AccountEntity;
@@ -22,13 +23,23 @@ public class UserInMemoryDatabase {
         accountTable.put(account.idul(), account);
     }
 
+    public void insertIntoBuyerTable(BuyerEntity buyer) {
+        enforceAccountForeignKeyConstraint(buyer.idul());
+        buyerTable.put(buyer.idul(), buyer);
+    }
+
     public Optional<AccountEntity> selectFromAccountTable(Idul idul) {
         return Optional.ofNullable(accountTable.get(idul));
     }
 
-    public void insertIntoBuyerTable(BuyerEntity buyer) {
-        enforceAccountForeignKeyConstraint(buyer.idul());
-        buyerTable.put(buyer.idul(), buyer);
+    public Optional<AccountEntity> selectFromAccountTable(Email email) {
+        return accountTable.values().stream().filter(account -> account.email().equals(email))
+                .findFirst();
+    }
+
+    public Optional<BuyerEntity> selectFromBuyerTable(Idul idul) {
+        enforceAccountForeignKeyConstraint(idul);
+        return Optional.ofNullable(buyerTable.get(idul));
     }
 
     private void enforceAccountForeignKeyConstraint(Idul idul) {
