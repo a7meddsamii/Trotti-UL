e59970dfa25e37c41d60ce5c6521febe35c179d4
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-23 22:44:17 -0400

    Fix password that could not be rehydrated after storing in database

--- src/main/java/ca/ulaval/glo4003/trotti/domain/account/Password.java
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/account/Password.java
@@ -1,54 +1,47 @@
 package ca.ulaval.glo4003.trotti.domain.account;
 
 import ca.ulaval.glo4003.trotti.domain.shared.exception.InvalidParameterException;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Objects;
-import org.apache.commons.validator.routines.RegexValidator;
 
 public class Password {
-
-    private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{10,}$";
-    private static final RegexValidator REGEX_VALIDATOR = new RegexValidator(PASSWORD_PATTERN);
-
-    private final PasswordHasher hasher;
-    private final String value;
-
-    public Password(String value, PasswordHasher hasher) {
-        validate(value);
-        this.hasher = hasher;
-        this.value = hasher.hash(value);
-    }
-
-    public boolean matches(String rawPassword) {
-        return hasher.verify(rawPassword, this.value);
-    }
-
-    public String getHashedValue() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Password password = (Password) o;
-        return value.equals(password.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    private void validate(String password) {
-        if (!REGEX_VALIDATOR.isValid(password)) {
-            throw new InvalidParameterException(
-                    "Invalid password: it must contain at least 10 characters, one uppercase letter, one digit, and one special character.");
-        }
-    }
+	private final PasswordHasher hasher;
+	private final String hashedValue;
+	
+	public static Password fromHashed(String hashedPassword, PasswordHasher hasher) {
+		if (StringUtils.isBlank(hashedPassword)) {
+			throw new InvalidParameterException("Password cannot be null or empty.");
+		}
+		
+		return new Password(hashedPassword, hasher);
+	}
+	
+	private Password(String hashedValue, PasswordHasher hasher) {
+		this.hasher = hasher;
+		this.hashedValue = hashedValue;
+	}
+	
+	public boolean matches(String rawPassword) {
+		return hasher.verify(rawPassword, this.hashedValue);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Password password = (Password) o;
+		return hashedValue.equals(password.hashedValue);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(hashedValue);
+	}
+	
+	@Override
+	public String toString() {
+		return hashedValue;
+	}
 }
