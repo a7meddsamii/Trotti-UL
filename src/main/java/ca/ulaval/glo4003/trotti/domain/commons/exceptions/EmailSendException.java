edeb3ab2d70e9795517d2913c6f8f26b5aa24737
samy <sakha65@ulaval.ca>
2025-09-23 22:10:20 -0400

    feat: added exception

---
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/commons/exceptions/EmailSendException.java
@@ -0,0 +1,7 @@
+package ca.ulaval.glo4003.trotti.domain.commons.exceptions;
+
+public class EmailSendException extends RuntimeException{
+    public EmailSendException(String description) {
+        super(description);
+    }
+}
