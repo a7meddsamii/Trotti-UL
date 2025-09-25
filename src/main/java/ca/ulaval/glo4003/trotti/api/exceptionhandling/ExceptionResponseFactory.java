237b0241abb174ad733d963508281332d5367481
moussa <ali.moussa.1@ulaval.ca>
2025-09-25 07:28:07 -0400

    exception mapper

---
+++ src/main/java/ca/ulaval/glo4003/trotti/api/exceptionhandling/ExceptionResponseFactory.java
@@ -0,0 +1,12 @@
+package ca.ulaval.glo4003.trotti.api.exceptionhandling;
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+public class ExceptionResponseFactory {
+    public static Response errorResponse(Response.Status status, String message) {
+        ApiErrorResponse response = new ApiErrorResponse(message);
+
+        return Response.status(status).type(MediaType.APPLICATION_JSON).entity(response).build();
+    }
+}
--- src/main/java/ca/ulaval/glo4003/trotti/api/exceptionhandling/InvalidParameterExceptionMapper.java
+++ src/main/java/ca/ulaval/glo4003/trotti/api/exceptionhandling/InvalidParameterExceptionMapper.java
@@ -1,19 +1,15 @@
 package ca.ulaval.glo4003.trotti.api.exceptionhandling;
 
 import ca.ulaval.glo4003.trotti.domain.shared.exception.InvalidParameterException;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class ApiExceptionHandler implements ExceptionMapper<InvalidParameterException> {
-
+public class InvalidParameterExceptionMapper implements ExceptionMapper<InvalidParameterException> {
     @Override
     public Response toResponse(InvalidParameterException exception) {
-        ApiErrorResponse errorResponse = new ApiErrorResponse(exception.getMessage());
-
-        return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON)
-                .entity(errorResponse).build();
+        return ExceptionResponseFactory.errorResponse(Response.Status.BAD_REQUEST,
+                exception.getMessage());
     }
 }
