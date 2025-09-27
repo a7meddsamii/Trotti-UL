d98b47878229e4e8ed6ed81ff1c6b8597625c1bb
moussa <ali.moussa.1@ulaval.ca>
2025-09-27 17:47:26 -0400

    refactor password:delegate a rawPassword validation at this class

--- src/main/java/ca/ulaval/glo4003/trotti/domain/account/Password.java
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/account/Password.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.trotti.domain.account;
 
-import ca.ulaval.glo4003.trotti.domain.shared.exception.InvalidParameterException;
+import ca.ulaval.glo4003.trotti.domain.commons.exceptions.InvalidParameterException;
 import java.util.Objects;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.RegexValidator;
 
 public class Password {
+    private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{10,}$";
+    private static final RegexValidator REGEX_VALIDATOR = new RegexValidator(PASSWORD_PATTERN);
+
     private final PasswordHasher hasher;
     private final String hashedValue;
 
@@ -13,15 +17,16 @@ public class Password {
     }
 
     private Password(String hashedValue, PasswordHasher hasher) {
-        validate(hashedValue);
+        validateNotBlank(hashedValue);
         this.hasher = hasher;
         this.hashedValue = hashedValue;
     }
 
-    private void validate(String hashedPassword) {
-        if (StringUtils.isBlank(hashedPassword)) {
-            throw new InvalidParameterException("Password cannot be null or empty.");
-        }
+    public static Password createNew(String plainPassword, PasswordHasher hasher) {
+        validateNotBlank(plainPassword);
+        ensurePasswordMeetsPolicy(plainPassword);
+        String hashedPassword = hasher.hash(plainPassword);
+        return new Password(hashedPassword, hasher);
     }
 
     public boolean matches(String rawPassword) {
@@ -45,5 +50,18 @@ public class Password {
     @Override
     public String toString() {
         return hashedValue;
+    }
+
+    private static void ensurePasswordMeetsPolicy(String password) {
+        if (!REGEX_VALIDATOR.isValid(password)) {
+            throw new InvalidParameterException(
+                    "Invalid password: it must contain at least 10 characters, one uppercase letter, one digit, and one special character.");
+        }
+    }
+
+    private static void validateNotBlank(String hashedPassword) {
+        if (StringUtils.isBlank(hashedPassword)) {
+            throw new InvalidParameterException("Password cannot be null or empty.");
+        }
     }
 }
