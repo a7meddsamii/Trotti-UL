242d4275384ead36106b66f1d35dd490ee718f0a
moussa <ali.moussa.1@ulaval.ca>
2025-09-27 17:46:12 -0400

    delete mapper class and test and renamed service to AccountApplicationService

--- src/main/java/ca/ulaval/glo4003/trotti/domain/commons/exceptions/AlreadyExistsException.java
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/commons/exceptions/AlreadyExistsException.java
@@ -1,4 +1,4 @@
-package ca.ulaval.glo4003.trotti.domain.shared.exception;
+package ca.ulaval.glo4003.trotti.domain.commons.exceptions;
 
 public class AlreadyExistsException extends RuntimeException {
     public AlreadyExistsException(String description) {
