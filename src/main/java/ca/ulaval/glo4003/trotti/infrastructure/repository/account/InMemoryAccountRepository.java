02c636c6f5ee1c27fee575e0fae2532c6b477d06
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-27 20:25:49 -0400

    Add an account mapper

--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/InMemoryAccountRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/InMemoryAccountRepository.java
@@ -5,40 +5,33 @@ import ca.ulaval.glo4003.trotti.domain.account.Email;
 import ca.ulaval.glo4003.trotti.domain.account.Idul;
 import ca.ulaval.glo4003.trotti.domain.account.repository.AccountRepository;
 import ca.ulaval.glo4003.trotti.infrastructure.repository.UserInMemoryDatabase;
+
 import java.util.Optional;
 
 public class InMemoryAccountRepository implements AccountRepository {
-    private final UserInMemoryDatabase database;
-
-    public InMemoryAccountRepository(UserInMemoryDatabase database) {
-        this.database = database;
-    }
-
-    @Override
-    public void save(Account account) {
-        AccountEntity accountEntity =
-                new AccountEntity(account.getIdul(), account.getName(), account.getBirthDate(),
-                        account.getGender(), account.getEmail(), account.getPassword());
-        this.database.insertIntoAccountTable(accountEntity);
-    }
-
-    @Override
-    public Optional<Account> findByEmail(Email email) {
-        Optional<AccountEntity> accountQuery = database.selectFromAccountTable(email);
-        return accountQuery.flatMap(this::mapResult);
-    }
-
-    @Override
-    public Optional<Account> findByIdul(Idul idul) {
-        Optional<AccountEntity> accountQuery = database.selectFromAccountTable(idul);
-        return accountQuery.flatMap(this::mapResult);
-    }
-
-    private Optional<Account> mapResult(AccountEntity accountFound) {
-        Account account =
-                new Account(accountFound.name(), accountFound.birthDate(), accountFound.gender(),
-                        accountFound.idul(), accountFound.email(), accountFound.password());
-
-        return Optional.of(account);
-    }
+	private final UserInMemoryDatabase database;
+	private final PersistenceAccountMapper accountMapper;
+	
+	public InMemoryAccountRepository(UserInMemoryDatabase database, PersistenceAccountMapper accountMapper) {
+		this.database = database;
+		this.accountMapper = accountMapper;
+	}
+	
+	@Override
+	public void save(Account account) {
+		AccountEntity accountEntity = accountMapper.toEntity(account);
+		this.database.insertIntoAccountTable(accountEntity);
+	}
+	
+	@Override
+	public Optional<Account> findByEmail(Email email) {
+		Optional<AccountEntity> accountQuery = database.selectFromAccountTable(email);
+		return accountQuery.flatMap(this.accountMapper::toDomain);
+	}
+	
+	@Override
+	public Optional<Account> findByIdul(Idul idul) {
+		Optional<AccountEntity> accountQuery = database.selectFromAccountTable(idul);
+		return accountQuery.flatMap(this.accountMapper::toDomain);
+	}
 }
---
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/PersistenceAccountMapper.java
@@ -0,0 +1,25 @@
+package ca.ulaval.glo4003.trotti.infrastructure.repository.account;
+
+import ca.ulaval.glo4003.trotti.domain.account.Account;
+
+import java.util.Optional;
+
+public class PersistenceAccountMapper {
+	
+	public AccountEntity toEntity(Account account) {
+		return new AccountEntity(
+				account.getIdul(), account.getName(), account.getBirthDate(),
+				account.getGender(), account.getEmail(), account.getPassword()
+		);
+	}
+	
+	public Optional<Account> toDomain(AccountEntity accountFound) {
+		Account account =
+				new Account(
+						accountFound.name(), accountFound.birthDate(), accountFound.gender(),
+						accountFound.idul(), accountFound.email(), accountFound.password()
+				);
+		
+		return Optional.of(account);
+	}
+}
