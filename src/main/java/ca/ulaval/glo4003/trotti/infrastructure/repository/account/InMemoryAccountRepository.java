465ba312e25f0cc4309cb78ac3bfce68b6b64666
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-28 22:44:57 -0400

    Apply code style

--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/InMemoryAccountRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/repository/account/InMemoryAccountRepository.java
@@ -1,9 +1,9 @@
 package ca.ulaval.glo4003.trotti.infrastructure.repository.account;
 
 import ca.ulaval.glo4003.trotti.domain.account.Account;
-import ca.ulaval.glo4003.trotti.domain.commons.Email;
 import ca.ulaval.glo4003.trotti.domain.account.Idul;
 import ca.ulaval.glo4003.trotti.domain.account.repository.AccountRepository;
+import ca.ulaval.glo4003.trotti.domain.commons.Email;
 import ca.ulaval.glo4003.trotti.infrastructure.mappers.AccountPersistenceMapper;
 import ca.ulaval.glo4003.trotti.infrastructure.repository.UserInMemoryDatabase;
 import java.util.Optional;
