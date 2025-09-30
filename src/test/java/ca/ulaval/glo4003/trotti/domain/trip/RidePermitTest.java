3153f17f0df16d0d44a46cf90bdba9c61de6a8f5
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-09-30 10:33:07 -0400

    add missing tests

--- src/test/java/ca/ulaval/glo4003/trotti/domain/trip/RidePermitTest.java
+++ src/test/java/ca/ulaval/glo4003/trotti/domain/trip/RidePermitTest.java
@@ -4,6 +4,7 @@ import ca.ulaval.glo4003.trotti.domain.account.values.Idul;
 import ca.ulaval.glo4003.trotti.domain.commons.Id;
 import ca.ulaval.glo4003.trotti.domain.order.Session;
 import java.time.LocalDate;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,17 +17,21 @@ class RidePermitTest {
 
     private static final LocalDate DATE_IN_SESSION = LocalDate.of(2020, 1, 1);
     private static final LocalDate DATE_OUT_OF_SESSION = LocalDate.of(2000, 12, 1);
+    private static final Idul IDUL = Idul.from("abcd");
+    private static final int LENGTH_OF_RANDOM_STRING = 10;
     private Session session;
     private RidePermit ridePermit;
+    private Id ridePermitId;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         session = Mockito.mock(Session.class);
-        ridePermit = new RidePermit(Id.randomId(), Idul.from("abdc"), session);
+        ridePermitId = Id.randomId();
+        ridePermit = new RidePermit(ridePermitId, IDUL, session);
     }
 
     @Test
-    public void givenDateWithinSession_whenIsActive_thenReturnTrue() {
+    void givenDateWithinSession_whenIsActive_thenReturnTrue() {
         Mockito.when(session.contains(DATE_IN_SESSION)).thenReturn(true);
 
         boolean response = ridePermit.isActiveFor(DATE_IN_SESSION);
@@ -35,7 +40,7 @@ class RidePermitTest {
     }
 
     @Test
-    public void givenDateOutOfSessionRange_whenIsActive_thenReturnFalse() {
+    void givenDateOutOfSessionRange_whenIsActive_thenReturnFalse() {
         Mockito.when(session.contains(DATE_OUT_OF_SESSION)).thenReturn(false);
 
         boolean response = ridePermit.isActiveFor(DATE_OUT_OF_SESSION);
@@ -43,4 +48,52 @@ class RidePermitTest {
         Assertions.assertFalse(response);
     }
 
+    @Test
+    void givenTwoEqualsRidePermits_whenEquals_thenReturnTrue() {
+        RidePermit ridePermit1 = new RidePermit(ridePermitId, IDUL, session);
+        RidePermit ridePermit2 = new RidePermit(ridePermitId, IDUL, session);
+
+        boolean response = ridePermit1.equals(ridePermit2);
+
+        Assertions.assertTrue(response);
+    }
+
+    @Test
+    void givenTwoRidePermitsWithDifferentId_whenEquals_thenReturnFalse() {
+        RidePermit ridePermit1 = new RidePermit(ridePermitId, IDUL, session);
+        RidePermit ridePermit2 = new RidePermit(Id.randomId(), IDUL, session);
+
+        boolean response = ridePermit1.equals(ridePermit2);
+
+        Assertions.assertFalse(response);
+    }
+
+    @Test
+    void givenTwoRidePermitsWithDifferentIdul_whenEquals_thenReturnFalse() {
+        RidePermit ridePermit1 = new RidePermit(ridePermitId, IDUL, session);
+        Idul differentIdul = Idul.from(RandomStringUtils.secure().next(LENGTH_OF_RANDOM_STRING));
+        RidePermit ridePermit2 = new RidePermit(ridePermitId, differentIdul, session);
+
+        boolean response = ridePermit1.equals(ridePermit2);
+
+        Assertions.assertFalse(response);
+    }
+
+    @Test
+    void givenTwoRidePermitsWithDifferentSession_whenEquals_thenReturnFalse() {
+        RidePermit ridePermit1 = new RidePermit(ridePermitId, IDUL, session);
+        Session differentSession = Mockito.mock(Session.class);
+        RidePermit ridePermit2 = new RidePermit(ridePermitId, IDUL, differentSession);
+
+        boolean response = ridePermit1.equals(ridePermit2);
+
+        Assertions.assertFalse(response);
+    }
+
+    @Test
+    void givenObjectOfDifferentType_whenEquals_thenReturnFalse() {
+        boolean response = ridePermit.equals(new Object());
+
+        Assertions.assertFalse(response);
+    }
 }
