5be5cc8883c1c379ee64d6b6357a3ef1f05ef697
a7meddsamii <ahmed.sami.1@ulaval.ca>
2025-09-30 00:09:41 -0400

    fix: remove unused InvalidSessionException.java

--- src/main/java/ca/ulaval/glo4003/trotti/domain/commons/exceptions/InvalidSessionException.java
+++
@@ -1,8 +0,0 @@
-package ca.ulaval.glo4003.trotti.domain.commons.exceptions;
-
-public class InvalidSessionException extends RuntimeException {
-
-    public InvalidSessionException(String message) {
-        super("INVALID_SESSION_EXCEPTION: " + message);
-    }
-}
