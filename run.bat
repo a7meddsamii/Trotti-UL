13c7f6e7689e97e065f0339740da7afa9931194b
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-10-03 09:55:50 -0400

    updating run script to not spam containers at each run

--- run.bat
+++ run.bat
@@ -16,14 +16,28 @@ set IMAGE_NAME=trottiul
 set RESOURCE_DIR=%cd%\src\main\resources\data\
 set EMPLOYEES_IDUL_FILE=Employe.e.s.csv
 set SEMESTER_FILE=semesters-252627.json
+set CONTAINER_NAME=trottiul_container
 
-REM Build the Docker image
+REM Stop and remove any existing container with the same name if it exists
+for /f "delims=" %%i in ('docker ps -aq -f name=%CONTAINER_NAME%') do (
+    echo Stopping and removing existing container...
+    docker stop %CONTAINER_NAME%
+    docker rm %CONTAINER_NAME%
+)
+
+REM Build the Docker image, failing if there are any errors
+echo Building Docker image...
 docker build -t %IMAGE_NAME% .
+if errorlevel 1 (
+    echo Error: Docker image build failed!
+    exit /b 1
+)
 
-REM Run the Docker container
+REM Run the Docker container with a specified container name
 docker run ^
     --env-file config.env ^
     -v "%RESOURCE_DIR%%EMPLOYEES_IDUL_FILE%:/app/data/Employe.e.s.csv" ^
     -v "%RESOURCE_DIR%%SEMESTER_FILE%:/app/data/semesters-252627.json" ^
+    --name %CONTAINER_NAME% ^
     -p 8080:8080 ^
     %IMAGE_NAME%
--- run.sh
+++ run.sh
@@ -18,15 +18,40 @@ IMAGE_NAME="trottiul"
 RESOURCE_DIR="$(pwd)/src/main/resources/data/"
 EMPLOYEES_IDUL_FILE="Employe.e.s.csv"
 SEMESTER_FILE="semesters-252627.json"
+CONTAINER_NAME="trottiul_container"
 
+# stop and remove any existing container with the same name if it exists
+function stop_container() {
+    if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+        echo "Stopping and removing existing container..."
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
+    fi
+}
 
-# Build the Docker image
-docker build -t trottiul .
+# Run the Docker container with a specified container name
+function run_image() {
+    docker run \
+        --env-file config.env \
+        -v "$RESOURCE_DIR$EMPLOYEES_IDUL_FILE:/app/data/Employe.e.s.csv" \
+        -v "$RESOURCE_DIR$SEMESTER_FILE:/app/data/semesters-252627.json" \
+        --name $CONTAINER_NAME \
+        -p 8080:8080 \
+        $IMAGE_NAME
+}
 
-# Run the Docker container
-docker run \
-    --env-file config.env \
-    -v "$RESOURCE_DIR$EMPLOYEES_IDUL_FILE:/app/data/Employe.e.s.csv" \
-    -v "$RESOURCE_DIR$SEMESTER_FILE:/app/data/semesters-252627.json" \
-    -p 8080:8080 \
-    $IMAGE_NAME
+# Build the Docker image, failing if there are any errors
+function start_app() {
+    stop_container
+    echo "Building Docker image..."
+    docker build -t trottiul .
+    
+    if [ $? -ne 0 ]; then
+        echo "Error: Docker image build failed!"
+        exit 1
+    fi
+    
+    run_image
+}
+
+start_app
