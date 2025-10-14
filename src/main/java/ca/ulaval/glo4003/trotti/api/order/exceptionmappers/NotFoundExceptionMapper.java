2fae6c4190a74b3bd62aa4fcd15420441ec1f9b1
a7meddsamii <ahmed.sami.1@ulaval.ca>
2025-10-14 08:50:42 -0400

    feat: add not found exception mapper
    Co-authored-by: a7meddsamii <ahmed.sami.1@ulaval.ca>
    Co-authored-by: SamyKhalfallah <samy.khalfallah.1@ulaval.ca>

---
+++ src/main/java/ca/ulaval/glo4003/trotti/api/order/exceptionmappers/NotFoundExceptionMapper.java
@@ -0,0 +1,17 @@
+package ca.ulaval.glo4003.trotti.api.order.exceptionmappers;
+
+import ca.ulaval.glo4003.trotti.api.commons.ExceptionResponseFactory;
+import ca.ulaval.glo4003.trotti.domain.commons.exceptions.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        return ExceptionResponseFactory.errorResponse(Response.Status.NOT_FOUND, exception.getMessage());
+    }
+}
+
