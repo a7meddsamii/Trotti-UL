544b24495c88f3da0da1caef192bb13337c769ad
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-10-14 00:25:05 -0400

    improve scooter

--- src/main/java/ca/ulaval/glo4003/trotti/domain/trip/entities/Scooter.java
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/trip/entities/Scooter.java
@@ -2,8 +2,11 @@ package ca.ulaval.glo4003.trotti.domain.trip.entities;
 
 import ca.ulaval.glo4003.trotti.domain.commons.Id;
 import ca.ulaval.glo4003.trotti.domain.trip.exceptions.InvalidBatteryUpdate;
+import ca.ulaval.glo4003.trotti.domain.trip.exceptions.InvalidLocation;
 import ca.ulaval.glo4003.trotti.domain.trip.values.BatteryLevel;
 import ca.ulaval.glo4003.trotti.domain.trip.values.BatteryState;
+import ca.ulaval.glo4003.trotti.domain.trip.values.Location;
+
 import java.time.LocalDateTime;
 
 public class Scooter {
@@ -11,19 +14,23 @@ public class Scooter {
     private BatteryLevel batteryLevel;
     private BatteryState currentBatteryState;
     private LocalDateTime lastBatteryUpdate;
+	private Location stationLocation;
 
     public Scooter(
             Id id,
             BatteryLevel batteryLevel,
             LocalDateTime lastBatteryUpdate,
-            BatteryState currentBatteryState) {
+            BatteryState currentBatteryState,
+			Location stationLocation
+	) {
         this.id = id;
         this.batteryLevel = batteryLevel;
         this.lastBatteryUpdate = lastBatteryUpdate;
         this.currentBatteryState = currentBatteryState;
+		this.stationLocation = stationLocation;
     }
 
-    public void updateBatteryState(BatteryState newState, LocalDateTime dateTimeOfChange) {
+    private void changeBatteryState(BatteryState newState, LocalDateTime dateTimeOfChange) {
         if (dateTimeOfChange.isBefore(lastBatteryUpdate)) {
             throw new InvalidBatteryUpdate(
                     "The date of the battery state change cannot be before the last update.");
@@ -33,17 +40,38 @@ public class Scooter {
             return;
         }
 
-        this.batteryLevel =
-                newState.computeLevel(batteryLevel, lastBatteryUpdate, dateTimeOfChange);
+        this.batteryLevel = this.currentBatteryState.computeLevel(batteryLevel, lastBatteryUpdate, dateTimeOfChange);
         this.lastBatteryUpdate = dateTimeOfChange;
         this.currentBatteryState = newState;
     }
+	
+	public void dockAt(Location location, LocalDateTime dockingTime){
+		if (!this.stationLocation.isEmpty()) {
+			throw new InvalidLocation("scooter seems to be already docked at " + this.stationLocation);
+		}
+		
+		this.stationLocation = location;
+		changeBatteryState(BatteryState.CHARGING, dockingTime);
+	}
+	
+	public void undock(LocalDateTime undockingTime) {
+		if (this.stationLocation.isEmpty()) {
+			throw new InvalidLocation("scooter seems to already be undocked");
+		}
+		
+		this.stationLocation = Location.empty();
+		changeBatteryState(BatteryState.DISCHARGING, undockingTime);
+	}
 
-    public BatteryLevel getBattery() {
+    public BatteryLevel getBatteryLevel() {
         return batteryLevel;
     }
 
     public LocalDateTime getLastBatteryUpdate() {
         return lastBatteryUpdate;
     }
+	
+	public Location getStationLocation() {
+		return stationLocation;
+	}
 }
--- src/test/java/ca/ulaval/glo4003/trotti/domain/trip/scooter/entities/ScooterTest.java
+++ src/test/java/ca/ulaval/glo4003/trotti/domain/trip/scooter/entities/ScooterTest.java
@@ -3,10 +3,14 @@ package ca.ulaval.glo4003.trotti.domain.trip.scooter.entities;
 import ca.ulaval.glo4003.trotti.domain.commons.Id;
 import ca.ulaval.glo4003.trotti.domain.trip.entities.Scooter;
 import ca.ulaval.glo4003.trotti.domain.trip.exceptions.InvalidBatteryUpdate;
+import ca.ulaval.glo4003.trotti.domain.trip.exceptions.InvalidLocation;
 import ca.ulaval.glo4003.trotti.domain.trip.values.BatteryLevel;
 import ca.ulaval.glo4003.trotti.domain.trip.values.BatteryState;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import ca.ulaval.glo4003.trotti.domain.trip.values.Location;
+import ca.ulaval.glo4003.trotti.domain.trip.values.StationLocation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,40 +21,78 @@ public class ScooterTest {
     private static final Id AN_ID = Id.randomId();
     private static final BatteryLevel A_BATTERY_LEVEL = BatteryLevel.from(BigDecimal.valueOf(50));
     private static final LocalDateTime LAST_BATTERY_UPDATE = LocalDateTime.of(2024, 1, 1, 12, 0);
-    private static final LocalDateTime CURRENT_TIME = LocalDateTime.now();
-    private Scooter A_SCOOTER;
+    private static final LocalDateTime CURRENT_TIME = LocalDateTime.of(2024, 1, 1, 12, 30);
+	private static final LocalDateTime FUTURE_TIME = LocalDateTime.of(2024, 1, 1, 13, 0);
+	private static final Location A_LOCATION = new StationLocation("building A", "spot name");
+    private Scooter scooter;
+	
+	@BeforeEach
+	void setup() {
+		scooter = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING, A_LOCATION);
+	}
 
-    @BeforeEach
-    void setup() {
-        A_SCOOTER = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING);
-    }
-
+	@Test
+	void givenScooterAlreadyDocked_whenDockAt_thenThrowsException() {
+		Executable dockAt = () -> scooter.dockAt(A_LOCATION, CURRENT_TIME);
+		
+		Assertions.assertThrows(InvalidLocation.class, dockAt);
+	}
+	
+	@Test
+	void givenDockingTimeBeforeLastUpdate_whenDockAt_thenThrowsException(){
+		scooter = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING, Location.empty());
+		LocalDateTime dockingTime = LAST_BATTERY_UPDATE.minusMinutes(1);
+		
+		Executable dockAt = () -> scooter.dockAt(A_LOCATION, dockingTime);
+		
+		Assertions.assertThrows(InvalidBatteryUpdate.class, dockAt);
+	}
+	
     @Test
-    void givenUpdateTimeBeforeLastUpdate_whenUpdateBatteryState_thenThrowsException() {
-        LocalDateTime newUpdateTime = LAST_BATTERY_UPDATE.minusMinutes(1);
-
-        Executable updateState =
-                () -> A_SCOOTER.updateBatteryState(BatteryState.CHARGING, newUpdateTime);
-
-        Assertions.assertThrows(InvalidBatteryUpdate.class, updateState);
-    }
-
-    @Test
-    void givenSameState_whenUpdateBatteryState_thenDoesNotUpdateScooterInfo() {
-        A_SCOOTER = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING);
-
-        A_SCOOTER.updateBatteryState(BatteryState.CHARGING, CURRENT_TIME);
-
-        Assertions.assertSame(A_BATTERY_LEVEL, A_SCOOTER.getBattery());
-        Assertions.assertSame(LAST_BATTERY_UPDATE, A_SCOOTER.getLastBatteryUpdate());
-    }
-
-    @Test
-    void givenChargingState_whenUpdateBatteryState_thenComputeNewBatteryLevel() {
-        BatteryState newState = Mockito.mock(BatteryState.class);
-
-        A_SCOOTER.updateBatteryState(newState, CURRENT_TIME);
-
-        Mockito.verify(newState).computeLevel(A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, CURRENT_TIME);
-    }
+    void givenStationLocationAndDockingTime_whenDockAt_thenComputeNewBatteryLevel() {
+		scooter = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.DISCHARGING, Location.empty());
+		BatteryLevel expectedBatteryLevel = BatteryState.DISCHARGING.computeLevel(A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, CURRENT_TIME);
+		
+		scooter.dockAt(A_LOCATION, CURRENT_TIME);
+		
+		Assertions.assertEquals(expectedBatteryLevel, scooter.getBatteryLevel());
+	}
+		
+	
+	@Test
+	void givenStationLocation_whenDockAt_thenStationLocationIsSetToGivenLocation(){
+		scooter = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING, Location.empty());
+		
+		scooter.dockAt(A_LOCATION, CURRENT_TIME);
+		
+		Assertions.assertEquals(A_LOCATION, scooter.getStationLocation());
+	}
+	
+	@Test
+	void givenScooterAlreadyUndocked_whenUndock_thenThrowsException() {
+		scooter = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING, Location.empty());
+		
+		Executable undock = () -> scooter.undock(CURRENT_TIME);
+		
+		Assertions.assertThrows(InvalidLocation.class, undock);
+	}
+	
+	@Test
+	void givenUndockingTimeBeforeLastUpdate_whenUndock_thenThrowsException() {
+		LocalDateTime undockingTime = LAST_BATTERY_UPDATE.minusMinutes(1);
+		
+		Executable undock = () -> scooter.undock(undockingTime);
+		
+		Assertions.assertThrows(InvalidBatteryUpdate.class, undock);
+	}
+	
+	@Test
+	void givenStationLocationAndUndockingTime_whenUndock_thenComputeNewBatteryLevel() {
+		scooter = new Scooter(AN_ID, A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, BatteryState.CHARGING, A_LOCATION);
+		BatteryLevel expectedBatteryLevel = BatteryState.CHARGING.computeLevel(A_BATTERY_LEVEL, LAST_BATTERY_UPDATE, FUTURE_TIME);
+	
+		scooter.undock(FUTURE_TIME);
+		
+		Assertions.assertEquals(expectedBatteryLevel, scooter.getBatteryLevel());
+	}
 }
