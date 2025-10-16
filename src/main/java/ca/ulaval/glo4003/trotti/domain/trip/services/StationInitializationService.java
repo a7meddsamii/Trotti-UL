daabba6c2bf92943438358e8cc60633998dab2cd
Jérémie Blais <jereblais@gmail.com>
2025-10-15 21:57:40 -0400

    add StationInitializationService

---
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/trip/services/StationInitializationService.java
@@ -0,0 +1,36 @@
+package ca.ulaval.glo4003.trotti.domain.trip.services;
+
+import ca.ulaval.glo4003.trotti.domain.trip.entities.Scooter;
+import ca.ulaval.glo4003.trotti.domain.trip.entities.Station;
+import ca.ulaval.glo4003.trotti.domain.trip.factories.ScooterFactory;
+import ca.ulaval.glo4003.trotti.domain.trip.repositories.ScooterRepository;
+import ca.ulaval.glo4003.trotti.domain.trip.repositories.StationRepository;
+import java.util.List;
+
+public class StationInitializationService {
+
+    private final ScooterFactory scooterFactory;
+    private final StationRepository stationRepository;
+    private final ScooterRepository scooterRepository;
+
+    public StationInitializationService(
+            ScooterFactory scooterFactory,
+            StationRepository stationRepository,
+            ScooterRepository scooterRepository) {
+        this.scooterFactory = scooterFactory;
+        this.stationRepository = stationRepository;
+        this.scooterRepository = scooterRepository;
+    }
+
+    public void initializeStation(Station station) {
+        int initialScooterCount = station.getInitialScooterCount();
+        List<Scooter> scooters = scooterFactory.createScooters(initialScooterCount, station.getStationLocation());
+
+        scooters.forEach(scooter -> {
+            scooterRepository.save(scooter);
+            station.addScooter(scooter.getId());
+        });
+
+        stationRepository.save(station);
+    }
+}
