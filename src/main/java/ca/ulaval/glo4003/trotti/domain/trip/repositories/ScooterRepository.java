b40386147376a6f52189ea0cd4b77eac63b7a043
Jérémie Blais <jereblais@gmail.com>
2025-10-15 21:54:41 -0400

    add scooterRepo interface

---
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/trip/repositories/ScooterRepository.java
@@ -0,0 +1,12 @@
+package ca.ulaval.glo4003.trotti.domain.trip.repositories;
+
+import ca.ulaval.glo4003.trotti.domain.trip.entities.Scooter;
+
+public interface ScooterRepository {
+
+    void save(Scooter scooter);
+
+    Scooter findById(String scooterId);
+
+    void update(Scooter scooter);
+}
