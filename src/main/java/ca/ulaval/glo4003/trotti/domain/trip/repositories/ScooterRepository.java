217f02cd42fd6c448ff8378eb5234996ea5d441c
Jérémie Blais <jereblais@gmail.com>
2025-10-17 19:31:09 -0400

    adjust InMemoryScooterRepository

--- src/main/java/ca/ulaval/glo4003/trotti/domain/trip/repositories/ScooterRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/domain/trip/repositories/ScooterRepository.java
@@ -1,10 +1,11 @@
 package ca.ulaval.glo4003.trotti.domain.trip.repositories;
 
+import ca.ulaval.glo4003.trotti.domain.commons.Id;
 import ca.ulaval.glo4003.trotti.domain.trip.entities.Scooter;
 
 public interface ScooterRepository {
 
     void save(Scooter scooter);
 
-    Scooter findById(String scooterId);
+    Scooter findById(Id scooterId);
 }
--- src/main/java/ca/ulaval/glo4003/trotti/infrastructure/trip/repositories/InMemoryScooterRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/infrastructure/trip/repositories/InMemoryScooterRepository.java
@@ -1,22 +1,36 @@
 package ca.ulaval.glo4003.trotti.infrastructure.trip.repositories;
 
+import ca.ulaval.glo4003.trotti.domain.commons.Id;
 import ca.ulaval.glo4003.trotti.domain.trip.entities.Scooter;
 import ca.ulaval.glo4003.trotti.domain.trip.repositories.ScooterRepository;
+import ca.ulaval.glo4003.trotti.infrastructure.trip.mappers.ScooterPersistenceMapper;
+import ca.ulaval.glo4003.trotti.infrastructure.trip.repositories.records.ScooterRecord;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// placeholder
 public class InMemoryScooterRepository implements ScooterRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryScooterRepository.class);
 
-    @Override
-    public void save(Scooter scooter) {
-        LOGGER.info("Saving scooter {}", scooter.getId());
+    private final Map<Id, ScooterRecord> scooters = new HashMap<>();
+    private final ScooterPersistenceMapper scooterMapper;
+
+    public InMemoryScooterRepository(ScooterPersistenceMapper scooterMapper) {
+        this.scooterMapper = scooterMapper;
     }
 
     @Override
-    public Scooter findById(String scooterId) {
-        return null;
+    public void save(Scooter scooter) {
+        LOGGER.info("Saving scooter {}", scooter.getId());
+        ScooterRecord record = scooterMapper.toRecord(scooter);
+        scooters.put(scooter.getId(), record);
+    }
+
+    @Override
+    public Scooter findById(Id scooterId) {
+        ScooterRecord record = scooters.get(scooterId);
+        return record != null ? scooterMapper.toDomain(record) : null;
     }
 }
