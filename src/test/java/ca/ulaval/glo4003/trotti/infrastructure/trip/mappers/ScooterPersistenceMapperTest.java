8dda28d9f354e2bab832cac81d268ac9e184f3ed
Jérémie Blais <jereblais@gmail.com>
2025-10-17 19:42:51 -0400

    add ScooterPersistenceMapper tests

---
+++ src/test/java/ca/ulaval/glo4003/trotti/infrastructure/trip/mappers/ScooterPersistenceMapperTest.java
@@ -0,0 +1,48 @@
+package ca.ulaval.glo4003.trotti.infrastructure.trip.mappers;
+
+import ca.ulaval.glo4003.trotti.domain.commons.Id;
+import ca.ulaval.glo4003.trotti.domain.trip.entities.Scooter;
+import ca.ulaval.glo4003.trotti.fixtures.ScooterFixture;
+import ca.ulaval.glo4003.trotti.infrastructure.trip.repositories.records.ScooterRecord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ScooterPersistenceMapperTest {
+
+    private static final Id SCOOTER_ID = Id.randomId();
+
+    private ScooterPersistenceMapper scooterMapper;
+    private ScooterFixture scooterFixture;
+
+    @BeforeEach
+    void setup() {
+        scooterMapper = new ScooterPersistenceMapper();
+        scooterFixture = new ScooterFixture();
+    }
+
+    @Test
+    void givenScooter_whenToRecord_thenReturnCorrespondingScooterRecord() {
+        Scooter scooter = scooterFixture.build();
+
+        ScooterRecord record = scooterMapper.toRecord(scooter);
+
+        assertEqual(scooter, record);
+    }
+
+    @Test
+    void givenScooterRecord_whenToDomain_thenReturnCorrespondingScooter() {
+        Scooter scooter = scooterFixture.build();
+        ScooterRecord record = new ScooterRecord(scooter.getId(), scooter.getBattery(), scooter.getLocation());
+
+        Scooter resultScooter = scooterMapper.toDomain(record);
+
+        assertEqual(resultScooter, record);
+    }
+
+    private void assertEqual(Scooter scooter, ScooterRecord record) {
+        Assertions.assertEquals(scooter.getId(), record.id());
+        Assertions.assertEquals(scooter.getBattery(), record.battery());
+        Assertions.assertEquals(scooter.getLocation(), record.location());
+    }
+}
