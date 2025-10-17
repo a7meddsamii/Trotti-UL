faa67f3e8d77a41f2abff5b26ab141f72941e3ef
samy <sakha65@ulaval.ca>
2025-10-17 15:58:58 -0400

    spotless apply

--- src/test/java/ca/ulaval/glo4003/trotti/domain/trip/entities/TripTest.java
+++ src/test/java/ca/ulaval/glo4003/trotti/domain/trip/entities/TripTest.java
@@ -1,16 +1,13 @@
 package ca.ulaval.glo4003.trotti.domain.trip.entities;
 
-
 import ca.ulaval.glo4003.trotti.domain.account.values.Idul;
 import ca.ulaval.glo4003.trotti.domain.commons.Id;
 import ca.ulaval.glo4003.trotti.domain.trip.exceptions.TripException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.function.Executable;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class TripTest {
     private static final Id AN_ID = Id.randomId();
@@ -18,18 +15,16 @@ class TripTest {
     private static final LocalDateTime START_TIME = LocalDateTime.now();
     private static final LocalDateTime END_TIME = START_TIME.plusMinutes(1);
 
-
     private Trip trip;
-
 
     @BeforeEach
     public void setup() {
-        trip = new Trip(START_TIME,AN_ID,AN_IDUL,AN_ID);
+        trip = new Trip(START_TIME, AN_ID, AN_IDUL, AN_ID);
     }
 
     @Test
-    void givenTripWithNoEndTime_whenEnd_thenReturnNewTripWithEndTime(){
-        Trip trip = new Trip(START_TIME,AN_ID,AN_IDUL,AN_ID);
+    void givenTripWithNoEndTime_whenEnd_thenReturnNewTripWithEndTime() {
+        Trip trip = new Trip(START_TIME, AN_ID, AN_IDUL, AN_ID);
 
         Trip result = trip.end(END_TIME);
 
@@ -38,21 +33,21 @@ class TripTest {
     }
 
     @Test
-    void givenTripThatEnded_whenEnd_thenThrowsTripException(){
+    void givenTripThatEnded_whenEnd_thenThrowsTripException() {
         Trip endedTrip = trip.end(END_TIME);
 
-        Executable endingTrip = () ->  endedTrip.end(END_TIME);
+        Executable endingTrip = () -> endedTrip.end(END_TIME);
 
-        Assertions.assertThrows(TripException.class,endingTrip);
+        Assertions.assertThrows(TripException.class, endingTrip);
     }
 
     @Test
-    void givenEndTimeBeforeStartTime_whenEnd_thenThrowsTripException(){
+    void givenEndTimeBeforeStartTime_whenEnd_thenThrowsTripException() {
         LocalDateTime timeBeforeStart = START_TIME.minusMinutes(1);
 
         Executable endingTrip = () -> trip.end(timeBeforeStart);
 
-        Assertions.assertThrows(TripException.class,endingTrip);
+        Assertions.assertThrows(TripException.class, endingTrip);
     }
 
 }
