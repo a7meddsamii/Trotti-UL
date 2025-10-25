336d6a3bd3e1847c076efba34260fd0c8b3d9748
a7meddsamii <ahmed.sami.1@ulaval.ca>
2025-10-25 14:16:19 -0400

    feat: add swagger for authentication context

--- src/main/java/ca/ulaval/glo4003/trotti/api/authentication/dto/LoginRequest.java
+++ src/main/java/ca/ulaval/glo4003/trotti/api/authentication/dto/LoginRequest.java
@@ -1,8 +1,15 @@
 package ca.ulaval.glo4003.trotti.api.authentication.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(name = "LoginRequest", description = "Un request body pour s'authentifier")
 public record LoginRequest(
-    @NotBlank(message = "Email is required") String email,
-    @NotBlank(message = "Password is required") String password
+        @Schema(description = "Courriel Ulaval de l'utilisateur", example = "john.doe.1@ulaval.ca", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @Schema(description = "Mot de passe de l'utilisateur", example = "StrongP@ssw0rd!", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "Password is required")
+        String password
 ) {}
