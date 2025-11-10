6975a6274a7beb41982a35a15b0766f87b45a6a0
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-11-09 21:16:10 -0500

    fix PR request

--- src/main/java/ca/ulaval/glo4003/trotti/authentication/domain/values/Permission.java
+++ src/main/java/ca/ulaval/glo4003/trotti/authentication/domain/values/Permission.java
@@ -3,7 +3,7 @@ package ca.ulaval.glo4003.trotti.authentication.domain.values;
 import ca.ulaval.glo4003.trotti.authentication.domain.exception.AuthenticationException;
 
 public enum Permission {
-    ORDER_CONFIRM, CART_MODIFICATION, MAKE_TRIP, START_MAINTENANCE, END_MAINTENANCE, TRANSFER_SCOOTER, REQUEST_MAINTENANCE, CREATE_TECHNICIAN, DELETE_TECHNICIAN;
+    ORDER_CONFIRM, CART_MODIFICATION, MAKE_TRIP, START_MAINTENANCE, END_MAINTENANCE, TRANSFER_SCOOTER, REQUEST_MAINTENANCE, CREATE_EMPLOYEE, DELETE_EMPLOYEE;
 
     public static Permission fromString(String value) {
         for (Permission permission : Permission.values()) {
