e0a273646ca0859a9564f12f3647fe76c7c08ea2
Jérémie Blais <jereblais@gmail.com>
2025-11-18 22:12:55 -0500

    complete TransferApplicationService and tests

--- src/main/java/ca/ulaval/glo4003/trotti/trip/application/TransferApplicationService.java
+++ src/main/java/ca/ulaval/glo4003/trotti/trip/application/TransferApplicationService.java
@@ -4,39 +4,51 @@ import ca.ulaval.glo4003.trotti.account.domain.values.Idul;
 import ca.ulaval.glo4003.trotti.order.domain.values.SlotNumber;
 import ca.ulaval.glo4003.trotti.trip.domain.entities.Station;
 import ca.ulaval.glo4003.trotti.trip.domain.entities.Transfer;
-import ca.ulaval.glo4003.trotti.trip.domain.factories.TransferFactory;
 import ca.ulaval.glo4003.trotti.trip.domain.repositories.StationRepository;
 import ca.ulaval.glo4003.trotti.trip.domain.repositories.TransferRepository;
 import ca.ulaval.glo4003.trotti.trip.domain.values.Location;
+import ca.ulaval.glo4003.trotti.trip.domain.values.ScooterId;
 import ca.ulaval.glo4003.trotti.trip.domain.values.TransferId;
 import java.util.List;
 
 public class TransferApplicationService {
     private final TransferRepository transferRepository;
     private final StationRepository stationRepository;
-    private final TransferFactory transferFactory;
 
-    public TransferApplicationService(TransferRepository transferRepository, StationRepository stationRepository, TransferFactory transferFactory) {
+    public TransferApplicationService(TransferRepository transferRepository, StationRepository stationRepository) {
         this.transferRepository = transferRepository;
         this.stationRepository = stationRepository;
-        this.transferFactory = transferFactory;
     }
 
     public TransferId initiateTransfer(Location sourceStation, Idul technicianId, List<SlotNumber> sourceSlots) {
         Station station = stationRepository.findByLocation(sourceStation);
-        Transfer transfer = transferFactory.createTransfer(technicianId, station, sourceSlots);
+        Transfer transfer = Transfer.start(technicianId, sourceStation, station.retrieveScootersForTransfer(sourceSlots));
         
         transferRepository.save(transfer);
         return transfer.getTransferId();
     }
 
-    public void unloadScooters(TransferId transferId, Location destinationStation, List<SlotNumber> destinationSlots) {
+    public void unloadScooters(TransferId transferId, Idul technicianId, Location destinationStation, List<SlotNumber> destinationSlots) {
         Transfer transfer = transferRepository.findById(transferId);
         Station station = stationRepository.findByLocation(destinationStation);
         
-        transfer.unload(station, destinationSlots);
-        
+        List<ScooterId> unloadedScooters = transfer.unload(technicianId, destinationSlots.size());
+        for (int i = 0; i < unloadedScooters.size(); i++) {
+            station.returnScooter(destinationSlots.get(i), unloadedScooters.get(i));
+        }
         transferRepository.save(transfer);
         stationRepository.save(station);
+    }
+
+    public List<SlotNumber> findAvailableSlotsInStation(Location destinationStation) {
+        Station station = stationRepository.findByLocation(destinationStation);
+
+        return station.getAvailableSlots();
+    }
+
+    public List<SlotNumber> findOccupiedSlotsInStation(Location destinationStation) {
+        Station station = stationRepository.findByLocation(destinationStation);
+
+        return station.getOccupiedSlots();
     }
 }
---
+++ src/test/java/ca/ulaval/glo4003/trotti/trip/application/TransferApplicationServiceTest.java
@@ -0,0 +1,117 @@
+package ca.ulaval.glo4003.trotti.trip.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import ca.ulaval.glo4003.trotti.account.domain.values.Idul;
+import ca.ulaval.glo4003.trotti.order.domain.values.SlotNumber;
+import ca.ulaval.glo4003.trotti.trip.domain.entities.Station;
+import ca.ulaval.glo4003.trotti.trip.domain.entities.Transfer;
+import ca.ulaval.glo4003.trotti.trip.domain.repositories.StationRepository;
+import ca.ulaval.glo4003.trotti.trip.domain.repositories.TransferRepository;
+import ca.ulaval.glo4003.trotti.trip.domain.values.Location;
+import ca.ulaval.glo4003.trotti.trip.domain.values.ScooterId;
+import ca.ulaval.glo4003.trotti.trip.domain.values.TransferId;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class TransferApplicationServiceTest {
+
+    private static final Idul TECHNICIAN_ID = Idul.from("tech123");
+    private static final Location SOURCE_LOCATION = Location.of("Building A");
+    private static final Location DESTINATION_LOCATION = Location.of("Building B");
+
+    private TransferRepository transferRepository;
+    private StationRepository stationRepository;
+    private Station sourceStation;
+    private Station destinationStation;
+    private Transfer transfer;
+
+    private TransferApplicationService transferApplicationService;
+
+    @BeforeEach
+    void setup() {
+        transferRepository = Mockito.mock(TransferRepository.class);
+        stationRepository = Mockito.mock(StationRepository.class);
+        sourceStation = Mockito.mock(Station.class);
+        destinationStation = Mockito.mock(Station.class);
+        transfer = Mockito.mock(Transfer.class);
+
+        transferApplicationService = new TransferApplicationService(transferRepository, stationRepository);
+    }
+
+    @Test
+    void givenValidParameters_whenInitiateTransfer_thenTransferInitiated() {
+        List<SlotNumber> sourceSlots = List.of(new SlotNumber(1), new SlotNumber(2));
+        Set<ScooterId> scooters = Set.of(ScooterId.randomId());
+        Mockito.when(stationRepository.findByLocation(SOURCE_LOCATION)).thenReturn(sourceStation);
+        Mockito.when(sourceStation.retrieveScootersForTransfer(sourceSlots)).thenReturn(scooters);
+
+        TransferId transferId = transferApplicationService.initiateTransfer(SOURCE_LOCATION, TECHNICIAN_ID, sourceSlots);
+
+        Mockito.verify(transferRepository).save(Mockito.any(Transfer.class));
+        assertThat(transferId).isNotNull();
+    }
+
+    @Test
+    void givenValidParameters_whenUnloadScooters_thenTransferIsLoaded() {
+        TransferId transferId = TransferId.randomId();
+        List<SlotNumber> destinationSlots = List.of(new SlotNumber(1));
+        Mockito.when(transferRepository.findById(transferId)).thenReturn(transfer);
+        Mockito.when(stationRepository.findByLocation(DESTINATION_LOCATION)).thenReturn(destinationStation);
+        Mockito.when(transfer.unload(TECHNICIAN_ID, 1)).thenReturn(List.of());
+
+        transferApplicationService.unloadScooters(transferId, TECHNICIAN_ID, DESTINATION_LOCATION, destinationSlots);
+
+        Mockito.verify(transferRepository).findById(transferId);
+    }
+
+    @Test
+    void givenValidParameters_whenUnloadScooters_thenScootersAreReturnedToStation() {
+        TransferId transferId = TransferId.randomId();
+        List<SlotNumber> destinationSlots = List.of(new SlotNumber(1));
+        ScooterId scooterId = ScooterId.randomId();
+        Mockito.when(transferRepository.findById(transferId)).thenReturn(transfer);
+        Mockito.when(stationRepository.findByLocation(DESTINATION_LOCATION)).thenReturn(destinationStation);
+        Mockito.when(transfer.unload(TECHNICIAN_ID, 1)).thenReturn(List.of(scooterId));
+
+        transferApplicationService.unloadScooters(transferId, TECHNICIAN_ID, DESTINATION_LOCATION, destinationSlots);
+
+        Mockito.verify(destinationStation).returnScooter(new SlotNumber(1), scooterId);
+    }
+
+    @Test
+    void givenValidParameters_whenUnloadScooters_thenTransferAndStationAreSaved() {
+        TransferId transferId = TransferId.randomId();
+        List<SlotNumber> destinationSlots = List.of(new SlotNumber(1));
+        Mockito.when(transferRepository.findById(transferId)).thenReturn(transfer);
+        Mockito.when(stationRepository.findByLocation(DESTINATION_LOCATION)).thenReturn(destinationStation);
+        Mockito.when(transfer.unload(TECHNICIAN_ID, 1)).thenReturn(List.of());
+
+        transferApplicationService.unloadScooters(transferId, TECHNICIAN_ID, DESTINATION_LOCATION, destinationSlots);
+
+        Mockito.verify(transferRepository).save(transfer);
+        Mockito.verify(stationRepository).save(destinationStation);
+    }
+
+    @Test
+    void givenValidLocation_whenFindAvailableSlots_thenStationIsLoaded() {
+        Mockito.when(stationRepository.findByLocation(DESTINATION_LOCATION)).thenReturn(destinationStation);
+
+        transferApplicationService.findAvailableSlotsInStation(DESTINATION_LOCATION);
+
+        Mockito.verify(stationRepository).findByLocation(DESTINATION_LOCATION);
+        Mockito.verify(destinationStation).getAvailableSlots();
+    }
+
+    @Test
+    void givenValidLocation_whenFindOccupiedSlots_thenStationIsLoaded() {
+        Mockito.when(stationRepository.findByLocation(DESTINATION_LOCATION)).thenReturn(destinationStation);
+
+        transferApplicationService.findOccupiedSlotsInStation(DESTINATION_LOCATION);
+
+        Mockito.verify(stationRepository).findByLocation(DESTINATION_LOCATION);
+        Mockito.verify(destinationStation).getOccupiedSlots();
+    }
+}
