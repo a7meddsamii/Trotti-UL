57135c8a7a7451017a62e9b018790801019c2952
Yannick André Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-11-19 22:51:10 -0500

    fix conflict

--- src/main/java/ca/ulaval/glo4003/trotti/commons/infrastructure/config/CommonsConfiguration.java
+++ src/main/java/ca/ulaval/glo4003/trotti/commons/infrastructure/config/CommonsConfiguration.java
@@ -2,6 +2,7 @@ package ca.ulaval.glo4003.trotti.commons.infrastructure.config;
 
 import ca.ulaval.glo4003.trotti.commons.infrastructure.config.loaders.DatabaseLoader;
 import ca.ulaval.glo4003.trotti.commons.infrastructure.config.loaders.EventBusLoader;
+import ca.ulaval.glo4003.trotti.commons.infrastructure.config.loaders.InitialAdminLoader;
 import ca.ulaval.glo4003.trotti.config.Configuration;
 
 public class CommonsConfiguration extends Configuration {
