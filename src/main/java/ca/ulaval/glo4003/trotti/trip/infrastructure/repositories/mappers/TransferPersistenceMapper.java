8a35489a4a484d0848bf283248c414a8586be954
Jérémie Blais <jereblais@gmail.com>
2025-11-19 19:23:29 -0500

    fix toDomain in persistenceMapper

--- src/main/java/ca/ulaval/glo4003/trotti/trip/infrastructure/repositories/mappers/TransferPersistenceMapper.java
+++ src/main/java/ca/ulaval/glo4003/trotti/trip/infrastructure/repositories/mappers/TransferPersistenceMapper.java
@@ -1,7 +1,11 @@
 package ca.ulaval.glo4003.trotti.trip.infrastructure.repositories.mappers;
 
+import ca.ulaval.glo4003.trotti.account.domain.values.Idul;
 import ca.ulaval.glo4003.trotti.trip.domain.entities.Transfer;
+import ca.ulaval.glo4003.trotti.trip.domain.values.Location;
+import ca.ulaval.glo4003.trotti.trip.domain.values.TransferId;
 import ca.ulaval.glo4003.trotti.trip.infrastructure.repositories.records.TransferRecord;
+import java.util.HashMap;
 
 public class TransferPersistenceMapper {
 
@@ -11,7 +15,11 @@ public class TransferPersistenceMapper {
     }
 
     public Transfer toDomain(TransferRecord record) {
-        return Transfer.rehydrate(record.transferId(), record.location(), record.technicianId(),
-                record.scootersMoved());
+        return Transfer.rehydrate(
+                TransferId.from(record.transferId().toString()),
+                Location.of(record.location().toString()),
+                Idul.from(record.technicianId().toString()),
+                new HashMap<>(record.scootersMoved())
+        );
     }
 }
