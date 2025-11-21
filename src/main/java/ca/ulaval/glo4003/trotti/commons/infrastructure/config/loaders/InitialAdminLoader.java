2d7615aa85d07cd8a465d73f939a042e9e64abc9
riri2000 <74474873+riri2000@users.noreply.github.com>
2025-11-20 21:27:24 -0500

    Use run.sh to read the config file!

--- src/main/java/ca/ulaval/glo4003/trotti/commons/infrastructure/config/loaders/InitialAdminLoader.java
+++ src/main/java/ca/ulaval/glo4003/trotti/commons/infrastructure/config/loaders/InitialAdminLoader.java
@@ -5,14 +5,21 @@ import ca.ulaval.glo4003.trotti.account.domain.factories.AccountFactory;
 import ca.ulaval.glo4003.trotti.account.domain.repositories.AccountRepository;
 import ca.ulaval.glo4003.trotti.account.domain.services.PasswordHasher;
 import ca.ulaval.glo4003.trotti.account.domain.values.*;
-import ca.ulaval.glo4003.trotti.account.domain.values.permissions.AccountPermissions;
-import ca.ulaval.glo4003.trotti.account.domain.values.permissions.Permission;
-import ca.ulaval.glo4003.trotti.commons.infrastructure.config.EnvironmentReader;
+import ca.ulaval.glo4003.trotti.account.domain.values.Permission;
+import ca.ulaval.glo4003.trotti.commons.domain.Idul;
 import ca.ulaval.glo4003.trotti.config.bootstrapper.Bootstrapper;
+
 import java.time.LocalDate;
 import java.util.Set;
 
 public class InitialAdminLoader extends Bootstrapper {
+
+    private static final String INITIAL_ADMIN_NAME = "ADMIN_NAME";
+    private static final String INITIAL_ADMIN_EMAIL = "ADMIN_EMAIL";
+    private static final String INITIAL_ADMIN_IDUL = "ADMIN_IDUL";
+    private static final String INITIAL_ADMIN_BIRTHDATE = "ADMIN_BIRTHDATE";
+    private static final String INITIAL_ADMIN_GENDER = "ADMIN_GENDER";
+    private static final String INITIAL_ADMIN_PASSWORD = "ADMIN_PASSWORD";
 
     @Override
     public void load() {
@@ -26,17 +33,15 @@ public class InitialAdminLoader extends Bootstrapper {
             AccountFactory accountFactory = this.resourceLocator.resolve(AccountFactory.class);
             PasswordHasher passwordHasher = this.resourceLocator.resolve(PasswordHasher.class);
 
-            EnvironmentReader env = EnvironmentReader.getInstance();
+            String name = System.getenv(INITIAL_ADMIN_NAME);
+            LocalDate birthDate = LocalDate.parse(System.getenv(INITIAL_ADMIN_BIRTHDATE));
+            Gender gender = Gender.fromString(System.getenv(INITIAL_ADMIN_GENDER));
+            Idul idul = Idul.from(System.getenv(INITIAL_ADMIN_IDUL));
+            Email email = Email.from(System.getenv(INITIAL_ADMIN_EMAIL));
+            Password password = Password.fromPlain(System.getenv(INITIAL_ADMIN_PASSWORD), passwordHasher);
 
-            String name = env.get("ADMIN_NAME");
-            LocalDate birthDate = LocalDate.parse(env.get("ADMIN_BIRTHDATE"));
-            Gender gender = Gender.fromString(env.get("ADMIN_GENDER"));
-            Idul idul = Idul.from(env.get("ADMIN_IDUL"));
-            Email email = Email.from(env.get("ADMIN_EMAIL"));
-            Password password = Password.fromPlain(env.get("ADMIN_PASSWORD"), passwordHasher);
-
-            Set<Permission> initialAdminPermissions = Set.of(AccountPermissions.CREATE_TECHNICIAN,
-                    AccountPermissions.DELETE_TECHNICIAN);
+            Set<Permission> initialAdminPermissions = Set.of(Permission.CREATE_EMPLOYEE,
+                    Permission.DELETE_EMPLOYEE, Permission.CREATE_ADMIN, Permission.DELETE_ADMIN);
 
             Account admin = accountFactory.createInitialAdmin(name, birthDate, gender, idul, email,
                     password, Role.ADMIN, initialAdminPermissions);
