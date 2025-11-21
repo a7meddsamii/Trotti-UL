b16338f28bcd51b8bb791e594bd487f03c00246b
saelf9 <saelf9@ulaval.ca>
2025-11-21 01:45:36 -0500

    deleted old order files

--- src/main/java/ca/ulaval/glo4003/trotti/order/domain/exceptions/InvalidOrderException.java
+++
@@ -1,8 +0,0 @@
-package ca.ulaval.glo4003.trotti.order.domain.exceptions;
-
-public class InvalidOrderException extends RuntimeException {
-
-    public InvalidOrderException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ca/ulaval/glo4003/trotti/order/domain/exceptions/SessionException.java
+++
@@ -1,7 +0,0 @@
-package ca.ulaval.glo4003.trotti.order.domain.exceptions;
-
-public class SessionException extends RuntimeException {
-    public SessionException(String message) {
-        super(message);
-    }
-}
