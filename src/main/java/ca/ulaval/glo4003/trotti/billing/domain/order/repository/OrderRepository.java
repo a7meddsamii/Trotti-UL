fff3ccda9f60d2e979f5a577f8626917caaf83b9
saelf9 <saelf9@ulaval.ca>
2025-11-21 01:44:56 -0500

    repo for order and ride permit

--- src/main/java/ca/ulaval/glo4003/trotti/billing/domain/order/repository/OrderRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/billing/domain/order/repository/OrderRepository.java
@@ -1,14 +1,17 @@
 package ca.ulaval.glo4003.trotti.billing.domain.order.repository;
 
 import ca.ulaval.glo4003.trotti.billing.domain.order.entities.Order;
+import ca.ulaval.glo4003.trotti.billing.domain.order.values.OrderId;
 import ca.ulaval.glo4003.trotti.billing.domain.order.values.OrderStatus;
 import ca.ulaval.glo4003.trotti.commons.domain.Idul;
-import ca.ulaval.glo4003.trotti.order.domain.values.OrderId;
+
+import java.util.Optional;
+
 
 public interface OrderRepository {
     void save(Order order);
-
-    Order findById(OrderId orderId);
+    
+    Optional<Order> findById(OrderId orderId);
 
     boolean exists(Idul buyerId, OrderStatus status);
 }
--- src/main/java/ca/ulaval/glo4003/trotti/billing/domain/ridepermit/repository/RidePermitRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/billing/domain/ridepermit/repository/RidePermitRepository.java
@@ -5,11 +5,12 @@ import ca.ulaval.glo4003.trotti.billing.domain.ridepermit.values.RidePermitId;
 import ca.ulaval.glo4003.trotti.commons.domain.Idul;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface RidePermitRepository {
     void save(RidePermit order);
-
-    RidePermit findById(RidePermitId orderId);
+    
+    Optional<RidePermit> findById(RidePermitId orderId);
 
     List<RidePermit> findAllByIdul(Idul idul);
 
