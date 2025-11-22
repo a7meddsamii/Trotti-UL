76448a333c37c4e7003eccb00a68ff9db0c83f82
Yannick André Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-11-21 19:48:37 -0500

    remove RidePermitId heritage to Id class

--- src/main/java/ca/ulaval/glo4003/trotti/billing/domain/ridepermit/values/RidePermitId.java
+++ src/main/java/ca/ulaval/glo4003/trotti/billing/domain/ridepermit/values/RidePermitId.java
@@ -1,22 +1,52 @@
 package ca.ulaval.glo4003.trotti.billing.domain.ridepermit.values;
 
-import ca.ulaval.glo4003.trotti.commons.domain.Id;
+import ca.ulaval.glo4003.trotti.commons.domain.exceptions.InvalidParameterException;
 
-public class RidePermitId extends Id {
+import java.util.Objects;
+import java.util.UUID;
 
-    private RidePermitId() {
-        super();
-    }
+public class RidePermitId {
 
-    private RidePermitId(String value) {
-        super(value);
-    }
 
-    public static RidePermitId from(String value) {
-        return new RidePermitId(value);
-    }
-
-    public static RidePermitId randomId() {
-        return new RidePermitId();
-    }
+	private final UUID value;
+	
+	private RidePermitId() {
+		this.value = UUID.randomUUID();
+	}
+	
+	private RidePermitId(String value) {
+		try {
+			this.value = UUID.fromString(value);
+		} catch (IllegalArgumentException e) {
+			throw new InvalidParameterException("ID is not valid");
+		}
+	}
+	
+	public static RidePermitId from(String value) {
+		return new RidePermitId(value);
+	}
+	
+	public static RidePermitId randomId() {
+		return new RidePermitId();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		
+		RidePermitId id = (RidePermitId) o;
+		return value.equals(id.value);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
+	
+	@Override
+	public String toString() {
+		return value.toString();
+	}
 }
