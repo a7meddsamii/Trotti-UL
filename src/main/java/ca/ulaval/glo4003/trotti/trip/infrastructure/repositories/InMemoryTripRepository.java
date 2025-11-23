29508a2880284d16362bea77412616955a23d092
a7meddsamii <ahmed.sami.1@ulaval.ca>
2025-11-23 16:53:03 -0500

    fix: trip repository

--- src/main/java/ca/ulaval/glo4003/trotti/trip/infrastructure/repositories/InMemoryTripRepository.java
+++ src/main/java/ca/ulaval/glo4003/trotti/trip/infrastructure/repositories/InMemoryTripRepository.java
@@ -3,17 +3,16 @@ package ca.ulaval.glo4003.trotti.trip.infrastructure.repositories;
 import ca.ulaval.glo4003.trotti.commons.domain.Idul;
 import ca.ulaval.glo4003.trotti.trip.domain.entities.Trip;
 import ca.ulaval.glo4003.trotti.trip.domain.repositories.TripRepository;
+import ca.ulaval.glo4003.trotti.trip.domain.values.TripId;
 import ca.ulaval.glo4003.trotti.trip.domain.values.TripStatus;
 import ca.ulaval.glo4003.trotti.trip.infrastructure.repositories.mappers.TripPersistenceMapper;
 import ca.ulaval.glo4003.trotti.trip.infrastructure.repositories.records.TripRecord;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class InMemoryTripRepository implements TripRepository {
-    private Map<Idul, List<TripRecord>> tripTable = new HashMap<>();
+    private Map<TripId, TripRecord> tripTable = new HashMap<>();
     private final TripPersistenceMapper mapper;
 
     public InMemoryTripRepository(TripPersistenceMapper mapper) {
@@ -22,37 +21,32 @@ public class InMemoryTripRepository implements TripRepository {
 
     @Override
     public boolean exists(Idul idul, TripStatus status) {
-        if (tripTable.containsKey(idul)) {
-            return tripTable.get(idul).stream()
-                    .anyMatch(tripRecord -> tripRecord.tripStatus().equals(status));
-        }
+        return tripTable.values().stream()
+                    .anyMatch(tripRecord -> tripRecord.idul().equals(idul) &&
+                            tripRecord.tripStatus().equals(status));
 
-        return false;
     }
 
     @Override
     public void save(Trip trip) {
         TripRecord tripRecord = mapper.toRecord(trip);
-        tripTable.computeIfAbsent(tripRecord.idul(), idul -> new ArrayList<>()).add(tripRecord);
+        tripTable.put(tripRecord.tripId(), tripRecord);
     }
 
     @Override
     public List<Trip> findAllByIdul(Idul idul) {
-        if (tripTable.containsKey(idul)) {
-            return tripTable.get(idul).stream().map(mapper::toDomain).toList();
-        }
-
-        return Collections.emptyList();
+        return tripTable.values().stream()
+                .filter(tripRecord -> tripRecord.idul().equals(idul))
+                .map(mapper::toDomain)
+                .toList();
     }
 
     @Override
     public List<Trip> findBy(Idul idul, TripStatus tripStatus) {
-        if (tripTable.containsKey(idul)) {
-            return tripTable.get(idul).stream()
-                    .filter(tripRecord -> tripRecord.tripStatus().equals(tripStatus))
-                    .map(mapper::toDomain).toList();
-        }
-
-        return Collections.emptyList();
+        return tripTable.values().stream()
+                .filter(tripRecord -> tripRecord.idul().equals(idul) &&
+                        tripRecord.tripStatus().equals(tripStatus))
+                .map(mapper::toDomain)
+                .toList();
     }
 }
