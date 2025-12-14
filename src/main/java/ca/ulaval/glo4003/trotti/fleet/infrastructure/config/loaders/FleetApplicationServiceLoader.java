f430d8ee7a155bea9351676c0fadf238e1e4ff32
moussa <ali.moussa.1@ulaval.ca>
2025-12-14 11:11:11 -0500

    add loadFleetMaintenanceService add loadFleetMaintenanceService  at FleetApplicationServiceLoader

--- src/main/java/ca/ulaval/glo4003/trotti/fleet/infrastructure/config/loaders/FleetApplicationServiceLoader.java
+++ src/main/java/ca/ulaval/glo4003/trotti/fleet/infrastructure/config/loaders/FleetApplicationServiceLoader.java
@@ -1,15 +1,46 @@
 package ca.ulaval.glo4003.trotti.fleet.infrastructure.config.loaders;
 
+import ca.ulaval.glo4003.trotti.commons.domain.events.EventBus;
 import ca.ulaval.glo4003.trotti.config.bootstrapper.Bootstrapper;
 import ca.ulaval.glo4003.trotti.fleet.application.FleetApplicationService;
+import ca.ulaval.glo4003.trotti.fleet.application.FleetMaintenanceApplicationService;
+import ca.ulaval.glo4003.trotti.fleet.application.FleetOperationsApplicationService;
 import ca.ulaval.glo4003.trotti.fleet.application.TemporaryFleetApplicationService;
+import ca.ulaval.glo4003.trotti.fleet.domain.factories.TransferFactory;
 import ca.ulaval.glo4003.trotti.fleet.domain.repositories.FleetRepository;
+import ca.ulaval.glo4003.trotti.fleet.domain.repositories.TransferRepository;
 import java.time.Clock;
 
 public class FleetApplicationServiceLoader extends Bootstrapper {
     @Override
     public void load() {
+        loadFleetOperationsService();
+        loadFleetMaintenanceService();
         loadFleetApplicationService();
+    }
+
+    private void loadFleetOperationsService() {
+        FleetRepository fleetRepository = this.resourceLocator.resolve(FleetRepository.class);
+        Clock clock = this.resourceLocator.resolve(Clock.class);
+
+        FleetOperationsApplicationService service =
+                new FleetOperationsApplicationService(fleetRepository, clock);
+
+        this.resourceLocator.register(FleetOperationsApplicationService.class, service);
+    }
+
+    private void loadFleetMaintenanceService() {
+        FleetRepository fleetRepository = this.resourceLocator.resolve(FleetRepository.class);
+        TransferRepository transferRepository =
+                this.resourceLocator.resolve(TransferRepository.class);
+        TransferFactory transferFactory = this.resourceLocator.resolve(TransferFactory.class);
+        EventBus eventBus = this.resourceLocator.resolve(EventBus.class);
+        Clock clock = this.resourceLocator.resolve(Clock.class);
+
+        FleetMaintenanceApplicationService service = new FleetMaintenanceApplicationService(
+                fleetRepository, transferRepository, transferFactory, eventBus, clock);
+
+        this.resourceLocator.register(FleetMaintenanceApplicationService.class, service);
     }
 
     /**
